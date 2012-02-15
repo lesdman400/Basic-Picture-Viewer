f761be3e86b25bb2500f7ac62bd0d00cb7901e21
Leslie Mercier <Lesdman400@yahoo.com>
2012-02-15 11:05:02 -0500

    first commit

---
+++ imageViewer/imageViewer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.picBoxOne = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxOne)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnForward.Location = new System.Drawing.Point(258, 329);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(117, 23);
+            this.btnForward.TabIndex = 0;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = false;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnPrevious.Location = new System.Drawing.Point(12, 329);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(117, 23);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnExit.Location = new System.Drawing.Point(135, 329);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(117, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // picBoxOne
+            // 
+            this.picBoxOne.Image = global::WindowsFormsApplication1.Properties.Resources.lion;
+            this.picBoxOne.InitialImage = global::WindowsFormsApplication1.Properties.Resources.lion;
+            this.picBoxOne.Location = new System.Drawing.Point(38, 12);
+            this.picBoxOne.Name = "picBoxOne";
+            this.picBoxOne.Size = new System.Drawing.Size(304, 311);
+            this.picBoxOne.TabIndex = 3;
+            this.picBoxOne.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 384);
+            this.Controls.Add(this.picBoxOne);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnForward);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxOne)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox picBoxOne;
+    }
+}
+
---
+++ imageViewer/imageViewer/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int counter = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (counter==1)
+            {
+                picBoxOne.Image = Properties.Resources.lion;
+                counter--;
+
+
+            }
+            else if (counter==2)
+            {
+                picBoxOne.Image = Properties.Resources.tiger;
+                counter--;
+
+            }
+            else
+            {
+                picBoxOne.Image = Properties.Resources.woods;
+                counter = 2;
+            }
+
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            
+            if (counter==0){
+                picBoxOne.Image = Properties.Resources.lion;
+                counter++;
+                
+                
+            }
+            else if (counter==1){
+                picBoxOne.Image = Properties.Resources.tiger;
+                counter++;
+                
+            }
+            else
+            {
+                picBoxOne.Image = Properties.Resources.woods;
+                counter = 0;
+            }
+
+        }
+    }
+}
